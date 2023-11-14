a29010264f4500df4cad29dea65bece5de4c7c46
girish-devops-project <venkatareddygirish@gmail.com>
2023-11-13 21:43:25 -0500

    Enahnce entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,12 +4,30 @@ echo "==================="
 
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
+git config --global pull.ff only
 git config --global --add safe.directory /github/workspace
 
 python3 /app/readme-update.py
 
 git add -A && git commit -m "update the Readme"
-git pull origin main
-git push --set-upstream origin main
+
+# Get the current branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+echo "Current branch: $current_branch"
+
+# Attempt a non-interactive rebase
+git pull --rebase --no-edit origin "$current_branch"
+
+# Check if the rebase failed
+if [ $? -ne 0 ]; then
+    echo "Rebase failed. Falling back to merge."
+    # Abort the rebase
+    git rebase --abort
+    # Perform a non-interactive merge
+    git pull --no-edit origin "$current_branch"
+fi
+
+git push --set-upstream origin "$current_branch"
+
 
 echo "==================="
