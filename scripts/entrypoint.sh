02929865ac74354513f21c9ea5bf1b12efd9a0c8
girish-devops-project <venkatareddygirish@gmail.com>
2023-11-13 21:31:04 -0500

    Fix the entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,7 +6,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 readme-update.py
+python3 /app/readme-update.py
 
 git add -A && git commit -m "update the Readme"
 git push --set-upstream origin main
